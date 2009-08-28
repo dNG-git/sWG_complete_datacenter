c5dee9500ef323d642e3321c4c0624665b160eb4
@4u <>
2009-08-28 10:46:07 +0200

    Trivial: Added missing XML namespace definition. Change: Added "errors_datacenter_dir_physical_name_invalid"

--- data/settings/swg_datacenter.php
+++ data/settings/swg_datacenter.php
@@ -1,13 +1,13 @@
 <?xml version='1.0' encoding='UTF-8' ?>
-<swg_settings_file_v1>
+<swg_settings_file_v1 xmlns="urn:de.direct-netware.xmlns:swg.settings.v1">
 <phpexit><![CDATA[<?php exit (); ?>]]></phpexit>
 
 <datacenter value="1">
  <table value="swg_datacenter" />
 
  <path>
-	<icons value="data/themes/{var:theme}/" />
-	<upload value="data/uploads/" />
+  <icons value="data/themes/{var:theme}/" />
+  <upload value="data/uploads/" />
  </path>
 
  <media value="1">
@@ -15,20 +15,20 @@
  </media>
 
  <mimetype>
-	<iconize value="1" />
-	<unknown value="mimetype_unknown.png" />
+  <iconize value="1" />
+  <unknown value="mimetype_unknown.png" />
  </mimetype>
 
  <upload><files><max value="500" /></files></upload>
 
  <uploads>
-	<localside value="1" />
-	<serverside value="1" />
-	<sizeperfile value="1" />
-    <sizeperupload>
-     <local value="4" />
-     <server value="4" />
-    </sizeperupload>
+  <localside value="1" />
+  <serverside value="1" />
+  <sizeperfile value="1" />
+  <sizeperupload>
+   <local value="4" />
+   <server value="4" />
+  </sizeperupload>
  </uploads>
 </datacenter>
 
